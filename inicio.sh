78debec2e49c493ca87e3efb2f6cc3f18f8e4e23
sergio gummylonchas <sgomperez@gmail.com>
2023-05-26 15:13:37 +0000

    añadido control de inicio y destruccion en un solo file

--- inicio.sh
+++ inicio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+{
+
+if [ $1 = 1 ]; then
 #Creación de imágenes
 docker build -t bus:0.1 bus/
 docker build -t tienda:0.1 tienda/
@@ -11,3 +14,16 @@ docker run --name bus01 -p 7000:7000 -d bus:0.1
 docker run --name registro01 -p 6900:6900 -d registro:0.1
 docker run --name cajero01 -p 6901:6901 -d cajero:0.1
 docker run --name tienda01 -p 8888:8888 -d tienda:0.1
+elif [ $1 = 0 ]; then
+
+docker stop bus01 tienda01 registro01 cajero01
+docker rm bus01 tienda01 registro01 cajero01
+docker rmi bus tienda registro cajero
+
+fi
+
+} || {
+
+echo ERROR
+
+}
