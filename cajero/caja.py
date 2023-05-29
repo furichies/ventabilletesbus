8e74ee19c3f21ea0e89b35155de6426eb8bcbeb4
richifor <ricardo.instructor.formacion@gmail.com>
2023-05-29 17:39:30 +0000

    calidad código caja v.01

--- cajero/caja.py
+++ cajero/caja.py
@@ -15,13 +15,18 @@ class Usuario(db.Model):
 
 @app.route('/registro', methods=['POST'])
 def registro():
+    """
+    Registra un nuevo usuario.
+
+    :return: Mensaje de éxito o error en formato JSON.
+    """
     data = request.get_json()
     nombre = data.get('nombre')
     apellidos = data.get('apellidos')
 
     usuario_existente = Usuario.query.filter_by(nombre=nombre, apellidos=apellidos).first()
     if usuario_existente:
-        return jsonify({'mensaje': 'El usuario ya está registrado.'})
+        return jsonify({'mensaje': 'El usuario ya está registrado.'}), 400
 
     nuevo_usuario = Usuario(nombre=nombre, apellidos=apellidos, saldo=100)
     db.session.add(nuevo_usuario)
@@ -32,17 +37,27 @@ def registro():
 
 @app.route('/saldo', methods=['GET'])
 def consultar_saldo():
+    """
+    Consulta el saldo de un usuario.
+
+    :return: Saldo del usuario en formato JSON.
+    """
     nombre = request.args.get('nombre')
     usuario = Usuario.query.filter_by(nombre=nombre).first()
 
     if usuario:
         return jsonify({'saldo': usuario.saldo})
 
-    return jsonify({'mensaje': 'Usuario no encontrado.'})
+    return jsonify({'mensaje': 'Usuario no encontrado.'}), 404
 
 
 @app.route('/pagar', methods=['POST'])
 def realizar_pago():
+    """
+    Realiza un pago utilizando el saldo de un usuario.
+
+    :return: Mensaje de éxito o error en formato JSON.
+    """
     data = request.get_json()
     nombre = data.get('nombre')
     costo = data.get('costo')
@@ -55,13 +70,18 @@ def realizar_pago():
             db.session.commit()
             return jsonify({'mensaje': 'Pago realizado con éxito.', 'saldo': usuario.saldo})
         else:
-            return jsonify({'mensaje': 'Saldo insuficiente.'})
+            return jsonify({'mensaje': 'Saldo insuficiente.'}), 400
 
-    return jsonify({'mensaje': 'Usuario no encontrado.'})
+    return jsonify({'mensaje': 'Usuario no encontrado.'}), 404
 
 
 @app.route('/ingreso', methods=['POST'])
 def agregar_saldo():
+    """
+    Agrega saldo a un usuario existente.
+
+    :return: Mensaje de éxito o error en formato JSON.
+    """
     data = request.get_json()
     nombre = data.get('nombre')
     apellidos = data.get('apellidos')
@@ -74,7 +94,7 @@ def agregar_saldo():
         db.session.commit()
         return jsonify({'mensaje': 'Ingreso realizado con éxito.', 'saldo': usuario.saldo})
 
-    return jsonify({'mensaje': 'Usuario no encontrado.'})
+    return jsonify({'mensaje': 'Usuario no encontrado.'}), 404
 
 
 if __name__ == '__main__':
